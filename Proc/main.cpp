24b7cc3acc405b35a7d35c1a1035fe8c80668c30
W738806 <d.lake.smith@eagles.usm.edu>
2014-09-23 13:14:33 -0500

    Initial Commit

---
+++ Proc/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Proc
+//
+//  Created by Daniel Smith on 9/23/14.
+//  Copyright (c) 2014 dsmith86. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
